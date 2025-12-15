d5c88afb2b5acb0581b99dc531563fc7eb1db1fc
Fegann <mustafayevvfagan@gmail.com>
2025-12-15 14:51:08 -0500

    okey

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -oP '^ID=\K.*' /etc/os-release | head -1 | sed 's/"//g;s/kali-rolling/Kali/g;s/ubuntu/Ubuntu/g;s/debian/Debian/g;s/centos/CentOS/g;s/rhel/RHEL/g;s/fedora/Fedora/g;s/[[:lower:]]/\U&/g'
+lsb_release -i | cut -d: -f2 | tr -d '[:space:]'
