76e882a097f5a0383a9fe4a273f12649cd253815
Fegann <mustafayevvfagan@gmail.com>
2025-12-15 15:17:44 -0500

    okey

--- cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# Parametr yoxlaması: Əgər parametr daxil edilməyibsə, çıxış ver
 if [ -z "$1" ]; then
     echo "Usage: $0 <key_name>"
     exit 1
 fi
+
+# Parametrləri al
 KEY_NAME="$1"
-ssh-keygen -t rsa -b 4096 -f "$KEY_NAME" -N ""
+
+# RSA açar cütünü yaradın (4096 bitlik ölçü ilə)
+ssh-keygen -t rsa -b 4096 -f "$KEY_NAME" -N "" > /dev/null 2>&1
+
+# Çıxış məlumatı
+echo "Your identification has been saved in $KEY_NAME"
+echo "Your public key has been saved in $KEY_NAME.pub"
