45e0b9d0038d4f8f482ce132f560157c99f07e08
Fegann <mustafayevvfagan@gmail.com>
2026-01-18 09:58:45 -0500

    eoyk

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -X POST "$2" _H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" --data "$3"
+curl -s -X POST "$2" -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" --data "$3"
