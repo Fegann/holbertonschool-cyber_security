3cb03e95eaa137c65a17d9fc718e268ba731274b
Fegann <mustafayevvfagan@gmail.com>
2026-02-06 06:11:31 -0500

    oeky

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-# Generate a random 16-character string using OpenSSL
-random_value=$(openssl rand -base64 12 | tr -d '/+=' | cut -c1-16)
-
-# Combine the password and the random string
-combined_password="$1$random_value"
-
-# Generate SHA-512 hash of the combined password and save it in 3_hash.txt
-echo -n "$combined_password" | openssl dgst -sha512 > 3_hash.txt
+echo -n "$1$(openssl rand -base64 12 | tr -d '/+=' | cut -c1-16)" | openssl dgst -sha512 > 3_hash.txt
