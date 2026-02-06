280f4a7ff093453148a43baa5708dd57cb47d50e
Fegann <mustafayevvfagan@gmail.com>
2026-02-06 06:17:54 -0500

    oeky

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=rockyou.txt --format=raw-sha256 "$1"
-john --show "$1" > 4-password.txt
+john --wordlist=rockyou.txt --format=raw-sha256 "$1" | john --show "$1" > 4-password.txt
