217292677d4c45a405c3733bd1d4ef8a28df248d
Fegann <mustafayevfeqan71@gmail.com>
2026-04-15 06:39:00 +0000

    oeky

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -1000 auth.log | grep 'Failed password for' | awk '{print $10}' | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
+tail -1000 auth.log | grep 'Failed password for' | awk '{print $10}' | sort | uniq -c | sort -nr | head -2 | awk '{print $2}'
