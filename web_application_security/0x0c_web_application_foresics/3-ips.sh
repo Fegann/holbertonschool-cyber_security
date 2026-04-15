cc3a11b11181d33729ba06c76d9819d8accdfaa4
Fegann <mustafayevfeqan71@gmail.com>
2026-04-15 06:59:05 +0000

    oeky

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep 'Accepted' auth.log | awk '{for(i=1;i<=NF;i++) if($i=="from") print $(i+1)}' | sort | uniq | wc -l | awk '{print $1}'
+grep 'Accepted' auth.log | awk '{for(i=1;i<=NF;i++) if($i=="from") print $(i+1)}' | sort | uniq | wc -l 
