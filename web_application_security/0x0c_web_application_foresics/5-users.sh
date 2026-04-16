638f3216cb6e5eba94e3a9e7dc46fa9e3c6d0bcf
Fegann <mustafayevvfagan@gmail.com>
2026-04-16 15:58:47 -0400

    oeyk

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "new user" log_fayli.log | cut -d '=' -f 2 | cut -d ',' -f 1 | sort -u | tr '\n' ',' | sed 's/,$/\n/'
+grep "new user" auth.log | cut -d '=' -f 2 | cut -d ',' -f 1 | sort -u | tr '\n' ',' | sed 's/,$/\n/'
