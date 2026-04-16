44ced802b676f03c164492832ebfbcc62577d676
Fegann <mustafayevvfagan@gmail.com>
2026-04-16 15:49:25 -0400

    oeky

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "new user" auth.log | cut -d '=' -f 2 | cut -d ',' -f 1 | sort | tr '\n' ',' | sed 's/,$//'
+grep "new user" auth.log | cut -d '=' -f 2 | cut -d ',' -f 1 | sort | tr '\n' ','
